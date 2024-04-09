c7a463508ab4f4122e5b64a7b7042b93675519bb
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-04-09 06:30:53 -0700

    the start

---
+++ #CDRD final.py
@@ -0,0 +1,12 @@
+#CDRD final
+
+#this project will entaill a bunch of third party hacking options such as 
+#creraating a fake phone number
+#createing false creit cards
+#givbing a detailed description and all info of a person online
+#etc etc....
+
+import argparse
+import time
+import os
+import math
