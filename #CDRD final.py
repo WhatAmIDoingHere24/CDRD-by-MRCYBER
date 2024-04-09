bb058c97408f6138244d9dfbc4834262822dfdda
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-04-09 07:00:16 -0700

    Update #CDRD final.py

--- #CDRD final.py
+++ #CDRD final.py
@@ -10,3 +10,55 @@ import argparse
 import time
 import os
 import math
+
+#starup menu
+
+a='''
+ ▄████▄  ▓█████▄  ██▀███  ▓█████▄ 
+▒██▀ ▀█  ▒██▀ ██▌▓██ ▒ ██▒▒██▀ ██▌
+▒▓█    ▄ ░██   █▌▓██ ░▄█ ▒░██   █▌
+▒▓▓▄ ▄██▒░▓█▄   ▌▒██▀▀█▄  ░▓█▄   ▌
+▒ ▓███▀ ░░▒████▓ ░██▓ ▒██▒░▒████▓ 
+░ ░▒ ▒  ░ ▒▒▓  ▒ ░ ▒▓ ░▒▓░ ▒▒▓  ▒ 
+  ░  ▒    ░ ▒  ▒   ░▒ ░ ▒░ ░ ▒  ▒ 
+░         ░ ░  ░   ░░   ░  ░ ░  ░ 
+░ ░         ░       ░        ░    
+░         ░                ░      
+
+Welcome to CODE RED MR.CYBERS First toolkit
+
+The tool that contains a whole bunch of other peolples tools
+'''                                  
+print(a) 
+
+home = input('''
+1.SPAM
+----------
+2.FAKE OUT'S
+----------
+3.OISNT
+----------
+4.CUSTOM TOOL'S
+----------
+5.MISC.
+----------
+6.CREDIT
+----------
+
+''')
+
+spam = input('''
+1.BEAST BOMBER
+2.tool2
+3.tool3
+
+''')
+
+if(home == 1):
+    print(spam)
+if(spam == 1):
+    print(beast)
+
+
+
+
