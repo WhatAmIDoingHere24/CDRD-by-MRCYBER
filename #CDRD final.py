4742c4c112987afd668067f1b76d1a1df0f3614f
WhatAmIDoingHere24 <106035602+WhatAmIDoingHere24@users.noreply.github.com>
2024-05-02 06:01:37 -0700

    Update #CDRD final.py

--- #CDRD final.py
+++ #CDRD final.py
@@ -6,14 +6,17 @@
 #givbing a detailed description and all info of a person online
 #etc etc....
 
-import argparse
+#----------------------------
+
+#imports
+import tkinter as tk
 import time
 import os
 import math
 
 #starup menu
+'''
 
-a='''
  ▄████▄  ▓█████▄  ██▀███  ▓█████▄ 
 ▒██▀ ▀█  ▒██▀ ██▌▓██ ▒ ██▒▒██▀ ██▌
 ▒▓█    ▄ ░██   █▌▓██ ░▄█ ▒░██   █▌
@@ -25,40 +28,63 @@ a='''
 ░ ░         ░       ░        ░    
 ░         ░                ░      
 
-Welcome to CODE RED MR.CYBERS First toolkit
+Welcome to /CODE RED/ MR.CYBERS First toolkit
 
 The tool that contains a whole bunch of other peolples tools
-'''                                  
-print(a) 
+'''
 
-home = input('''
-1.SPAM
-----------
-2.FAKE OUT'S
-----------
-3.OISNT
-----------
-4.CUSTOM TOOL'S
-----------
-5.MISC.
-----------
-6.CREDIT
-----------
+cwd = os.getcwd()
 
-''')
+#tinker setup
+main = tk.Tk()
 
-spam = input('''
-1.BEAST BOMBER
-2.tool2
-3.tool3
+#this keeps events in check, its an event handler
 
-''')
+def cwd1(event):
+    print(cwd)
+def listdir(event):
+    os.listdir()
 
-if(home == 1):
-    print(spam)
-if(spam == 1):
-    print(beast)
+main.rowconfigure(0, minsize=50)
+main.columnconfigure([0, 1, 2, 3], minsize=50)
+
+button1 = tk.Button(text="CWD")
+button2 = tk.Button(text="LISTDIR")
 
 
+#labels
+lable1 = tk.Label(
+    text = "test1",
+    height=20,
+    width=40,
+    bg="black",
+    fg="white"
+
+)
+
+lable2 = tk.Label(
+    text = "this is some MORE text",
+    height=20,
+    width=40,
+    bg="white",
+    fg="black"
+
+)
+
+
+#binding lets buttons do functions
+button1.bind("<ButtonRelease-1>", cwd1)
+button2.bind("<ButtonRelease-1>", listdir)
+
+
+#you have to pack each element to use it 
+button1.grid(row=0,column=0)
+button2.grid(row=0,column=1,rowspan=2,columnspan=2)
+lable1.grid(row=2,column=0)
+lable2.grid(row=2,column=1)
+
+
+
+main.mainloop() #any code after this will not run until tk window is closed 
 
 
